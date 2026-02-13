c46646d3732b42b54e8284e648c022a8a26a4094
AndreiaBernardo <1andreia.lima.03@gmail.com>
2026-02-13 09:21:19 -0300

    feat: alteracao no footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="bg-emerald-950 text-white text-center p-4 mt-5">
       <div className="container">
-        <p className="mb-0">Feito por Andreia</p>
+        <p className="mb-0 ">Redes Sociais</p>
         
         <div className="text-white mt-2">
           <a href="https://github.com" target="_blank" rel="noopener noreferrer" className=" me-3">
@@ -13,6 +13,13 @@ const Footer = () => {
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" >
             <i className="bi bi-linkedin"></i>
           </a>
+        </div>
+
+        <div className="mt-4 ">
+          <p>Endereço</p>
+          <p>Rua das Flores, 123 - Jardim Botânico, Cidade Verde</p>
+          <p>Telefone: (11) 1234-5678</p>
+          <p>Email: contato@floresdobosque.com.br</p>
         </div>
       </div>
     </footer>
